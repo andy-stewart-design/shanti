d2203623ae849367721d5aea7d43cf591d7e7cdb
Andy Stewart <andy.stewart1170@gmail.com>
2022-08-25 07:42:43 -0700

    Feed Updates
    added function to extract metadata

--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -1,15 +1,22 @@
+// TODO: Fix Typescript
+
 import { readdirSync } from "fs";
 import { join } from "path";
 import Head from "next/head";
 import Container from "components/global/Container";
 import NextImage from "next/future/image";
 
-interface Props {
-  images: string[];
+interface Images {
+  slug: string;
+  client: string | undefined;
+  project: string | undefined;
 }
 
-const Feed = ({ images }: Props) => {
-  console.log(images);
+interface FeedProps {
+  images: Images;
+}
+
+const Feed = ({ images }: any) => {
   return (
     <>
       <Head>
@@ -20,16 +27,23 @@ const Feed = ({ images }: Props) => {
       <main>
         <Container t="xl">
           <div className="grid grid-cols-fit-sm sm:grid-cols-fit xl:grid-cols-fit-lg gap-4">
-            {images.map((img) => (
-              <div key={img} className="bg-black">
+            {images.map((img: Images) => (
+              <div
+                key={img.slug}
+                className="relative bg-black rounded-xl overflow-hidden"
+              >
                 <NextImage
-                  src={`/img/feed/${img}`}
+                  src={`/img/feed/${img.slug}`}
                   width="1000"
                   height="1000"
                   sizes="50vw"
                   quality="60"
                   alt="Feed image"
                 ></NextImage>
+                <div className="absolute bottom-0 left-0 grid grid-cols-2 w-full py-2 px-3 bg-gradient-to-t from-black/50 to-black/0">
+                  <p>{img.client}</p>
+                  <p>{img.project}</p>
+                </div>
               </div>
             ))}
           </div>
@@ -43,7 +57,20 @@ export const getStaticProps = async () => {
   const postsDir = join(process.cwd(), "public/img/feed");
   const images = readdirSync(postsDir);
 
-  return { props: { images } };
+  const getImageInfo = (array: string[]) => {
+    return array.map((slug) => {
+      const client = slug.match(/(?<=\().+?(?=\))/g);
+      const projectType = slug.match(/(?<=\[).+?(?=\])/g);
+
+      return {
+        slug,
+        project: projectType?.toString().replace("-", " "),
+        client: client?.toString().replace("-", " "),
+      };
+    });
+  };
+
+  return { props: { images: getImageInfo(images) } };
 };
 
 export default Feed;
